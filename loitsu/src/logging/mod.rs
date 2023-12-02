19d5cf02bcfd5b5dbc9b6bc664d260f7c6302d72
apeltsi <49206921+apeltsi@users.noreply.github.com>
2023-12-02 01:44:40 +0100

    Web log overlay

--- loitsu/src/logging/mod.rs
+++ loitsu/src/logging/mod.rs
@@ -1,6 +1,9 @@
 #[cfg(not(target_arch = "wasm32"))]
 use colored::*;
 
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+
 pub enum Color {
     Red,
     Yellow,
@@ -47,10 +50,19 @@ macro_rules! log_asset {
     ($($t:tt)*) => ($crate::logging::log("[LoiAsset]", $crate::logging::Color::Magenta, &format_args!($($t)*).to_string()))
 }
 
+#[cfg(target_arch = "wasm32")]
+#[wasm_bindgen]
+extern "C" {
+    fn add_log(prefix: String, prefix_color_hex: String, s: String);
+    fn add_warning(s: String);
+    fn add_error(s: String);
+}
+
 pub fn log(prefix: &str, prefix_color: Color, s: &str) {
     #[cfg(target_arch = "wasm32")]
     {
         web_sys::console::log_1(&s.into());
+        add_log(prefix.to_string(), color_hex(prefix_color), s.to_string());
     }
     #[cfg(not(target_arch = "wasm32"))]
     {
@@ -62,6 +74,7 @@ pub fn warn(s: &str) {
     #[cfg(target_arch = "wasm32")]
     {
         web_sys::console::warn_1(&s.into());
+        add_warning(s.to_string());
     }
     #[cfg(not(target_arch = "wasm32"))]
     {
@@ -73,6 +86,7 @@ pub fn error(s: &str) {
     #[cfg(target_arch = "wasm32")]
     {
         web_sys::console::error_1(&s.into());
+        add_error(s.to_string());
     }
     #[cfg(not(target_arch = "wasm32"))]
     {
@@ -94,5 +108,21 @@ fn colorize(s: &str, color: Color) -> String {
         Color::BrightCyan => s.bright_cyan().to_string(),
         Color::White => s.white().to_string(),
         Color::Black => s.black().to_string(),
+    }
+}
+#[cfg(target_arch = "wasm32")]
+fn color_hex(color: Color) -> String {
+    match color {
+        Color::Red => "#FF0000".to_string(),
+        Color::Yellow => "#FFFF00".to_string(),
+        Color::Green => "#169511".to_string(),
+        Color::BrightGreen => "#18B70F".to_string(),
+        Color::Blue => "#0536C9".to_string(),
+        Color::BrightBlue => "#3A71EA".to_string(),
+        Color::Magenta => "#7F198D".to_string(),
+        Color::Cyan => "#398CCB".to_string(),
+        Color::BrightCyan => "#61D6D6".to_string(),
+        Color::White => "#FFFFFF".to_string(),
+        Color::Black => "#000000".to_string(),
     }
 }
