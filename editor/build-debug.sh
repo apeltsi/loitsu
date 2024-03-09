5860a8b62508f3069f943b25a19a63dc4f9b3110
Amos Peltonen <49206921+apeltsi@users.noreply.github.com>
2024-03-09 19:52:17 +0100

    fix: editor buildscripts were outdated

---
+++ editor/build-debug.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cargo build --target wasm32-unknown-unknown
+wasm-bindgen ../target/wasm32-unknown-unknown/debug/loitsu-editor.wasm --target web --out-dir web/public/wasm
+cd web
+pnpm run build
+cd ../server
+cargo build
+cd ..
